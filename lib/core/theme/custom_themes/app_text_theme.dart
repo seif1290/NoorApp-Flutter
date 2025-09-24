534420a01d8450e3b10b394c03ac8a7f82fceaa8
Seif Elnawawy <elnawawyseif@gmail.com>
2025-09-24 14:48:58 +0300

    set all the text sizes and colors

--- lib/core/theme/custom_themes/app_text_theme.dart
+++ lib/core/theme/custom_themes/app_text_theme.dart
@@ -6,42 +6,67 @@ class AppTextTheme {
   AppTextTheme._();
 
   static const TextTheme _baseTextTheme = TextTheme(
-    headlineSmall: TextStyle(
-      fontFamily: AppFonts.amiri,
-      fontSize: 22,
-      fontWeight: FontWeight.bold,
-    ),
+    headlineLarge: TextStyle(fontFamily: AppFonts.cairo, fontSize: 30),
+    headlineSmall: TextStyle(fontFamily: AppFonts.cairo, fontSize: 20),
+    titleLarge: TextStyle(fontFamily: AppFonts.amiriQuran, fontSize: 18),
     titleMedium: TextStyle(fontFamily: AppFonts.cairo, fontSize: 16),
-    bodyMedium: TextStyle(fontFamily: AppFonts.cairo, fontSize: 16),
-    bodySmall: TextStyle(fontFamily: AppFonts.cairo, fontSize: 14),
+    bodyLarge: TextStyle(fontFamily: AppFonts.cairo, fontSize: 18),
+    labelLarge: TextStyle(fontFamily: AppFonts.cairo, fontSize: 18),
+    labelMedium: TextStyle(fontFamily: AppFonts.cairo, fontSize: 14),
+    labelSmall: TextStyle(fontFamily: AppFonts.cairo, fontSize: 12),
   );
 
   static final TextTheme lightTextTheme = _baseTextTheme.copyWith(
+    headlineLarge: _baseTextTheme.headlineLarge?.copyWith(
+      color: AppColors.lightTextPrimary,
+    ),
     headlineSmall: _baseTextTheme.headlineSmall?.copyWith(
-      color: AppColors.primary,
+      color: AppColors.lightTextPrimary,
+    ),
+    titleLarge: _baseTextTheme.titleLarge?.copyWith(
+      color: AppColors.lightTextPrimary,
     ),
     titleMedium: _baseTextTheme.titleMedium?.copyWith(
       color: AppColors.lightTextPrimary,
     ),
-    bodyMedium: _baseTextTheme.bodyMedium?.copyWith(
+    bodyLarge: _baseTextTheme.bodyLarge?.copyWith(
       color: AppColors.lightTextSecondary,
     ),
-    bodySmall: _baseTextTheme.bodySmall?.copyWith(
+    labelLarge: _baseTextTheme.labelLarge?.copyWith(
+      color: AppColors.lightSurface,
+    ),
+    labelMedium: _baseTextTheme.labelMedium?.copyWith(
+      color: AppColors.lightTextSecondary,
+    ),
+    labelSmall: _baseTextTheme.labelSmall?.copyWith(
       color: AppColors.lightTextSecondary,
     ),
   );
 
+  // Dark Text Theme
   static final TextTheme darkTextTheme = _baseTextTheme.copyWith(
+    headlineLarge: _baseTextTheme.headlineLarge?.copyWith(
+      color: AppColors.darkTextPrimary,
+    ),
     headlineSmall: _baseTextTheme.headlineSmall?.copyWith(
-      color: AppColors.secondary,
+      color: AppColors.darkTextPrimary,
+    ),
+    titleLarge: _baseTextTheme.titleLarge?.copyWith(
+      color: AppColors.darkTextPrimary,
     ),
     titleMedium: _baseTextTheme.titleMedium?.copyWith(
       color: AppColors.darkTextPrimary,
     ),
-    bodyMedium: _baseTextTheme.bodyMedium?.copyWith(
+    bodyLarge: _baseTextTheme.bodyLarge?.copyWith(
       color: AppColors.darkTextSecondary,
     ),
-    bodySmall: _baseTextTheme.bodySmall?.copyWith(
+    labelLarge: _baseTextTheme.labelLarge?.copyWith(
+      color: AppColors.darkSurface,
+    ),
+    labelMedium: _baseTextTheme.labelMedium?.copyWith(
+      color: AppColors.darkTextSecondary,
+    ),
+    labelSmall: _baseTextTheme.labelSmall?.copyWith(
       color: AppColors.darkTextSecondary,
     ),
   );
