0b5a20e0d40dc350f2d9be5a0565191b3b8b1163
Seif Elnawawy <elnawawyseif@gmail.com>
2025-10-02 11:27:46 +0300

    initialize the dependecies of home feature

--- lib/core/di/setup.dart
+++ lib/core/di/setup.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:noor/core/network/dio_handler.dart';
 import 'package:noor/core/services/shared_prefs_service.dart';
+import 'package:noor/core/simple_bloc_observer.dart';
+import 'package:noor/features/home/data/data_sources/quran_data_source.dart';
+import 'package:noor/features/home/data/repos/quran_repo.dart';
+import 'package:noor/features/home/data/repos/quran_repo_impl.dart';
 import 'package:noor/features/onboarding/data/repos/onboarding_repo.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,6 +14,9 @@ final GetIt getIt = GetIt.instance;
 
 Future<void> setup() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
+  // Dio
+  DioHandler.setup();
   // Shared Preferences
   final prefs = await SharedPreferences.getInstance();
   getIt.registerLazySingleton<SharedPrefsService>(
@@ -23,8 +32,12 @@ Future<void> setup() async {
 
 void _initHome() {
   // Data Sources
-
+  getIt.registerLazySingleton<QuranDataSource>(
+    () => QuranDataSourceImpl(dio: getIt.get<DioHandler>().dio),
+  );
   // Repos
-
+  getIt.registerLazySingleton<QuranRepo>(
+    () => QuranRepoImpl(quranDataSource: getIt.get<QuranDataSource>()),
+  );
   // UseCases
 }
