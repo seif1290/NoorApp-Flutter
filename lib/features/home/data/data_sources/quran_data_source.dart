c8411b26d65c52597ee7bb2b21a007b3025bd168
Seif Elnawawy <elnawawyseif@gmail.com>
2025-10-02 11:26:55 +0300

    created QuranDataSource class to get all the quran

---
+++ lib/features/home/data/data_sources/quran_data_source.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:noor/core/error_handling/dio_exception_handler.dart';
+import 'package:noor/core/error_handling/network_exception.dart';
+import 'package:noor/core/network/network_constants.dart';
+import 'package:noor/features/home/data/models/surah_model.dart';
+
+abstract class QuranDataSource {
+  Future<List<SurahModel>> getQuran();
+}
+
+class QuranDataSourceImpl implements QuranDataSource {
+  final Dio _dio;
+  QuranDataSourceImpl({required Dio dio}) : _dio = dio;
+
+  @override
+  Future<List<SurahModel>> getQuran() async {
+    try {
+      final response = await _dio.get(NetworkConstants.quranEndPoint);
+      return (response.data['data']['surahs'] as List)
+          .map((surah) => SurahModel.fromJson(surah))
+          .toList();
+    } on DioException catch (e) {
+      throw DioExceptionHandler.handle(e);
+    } catch (e) {
+      throw NetworkException(
+        NetworkExceptionType.unknown,
+        message: e.toString(),
+      );
+    }
+  }
+}
