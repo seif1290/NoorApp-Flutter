3a80254eed052ebad6312c6cc1081c4f41484290
Seif Elnawawy <elnawawyseif@gmail.com>
2025-12-08 11:58:41 +0200

    - created reciters cubit - made the audio player cubit get the current reciter

--- lib/features/home/presentation/view_models/audio_player_cubit/audio_player_cubit.dart
+++ lib/features/home/presentation/view_models/audio_player_cubit/audio_player_cubit.dart
@@ -5,19 +5,24 @@ import 'package:noor/core/error_handling/failure.dart';
 import 'package:noor/features/home/data/models/surah_model/surah_model.dart';
 import 'package:noor/features/home/data/repos/audio_repo.dart';
 import 'package:noor/features/home/domain/use_cases/load_surah_with_audio_use_case.dart';
+import 'package:noor/core/data/models/reciter_model/reciter_model.dart';
+import 'package:noor/features/settings/data/repos/reciters_repo.dart';
 
 part 'audio_player_state.dart';
 part 'audio_player_cubit.freezed.dart';
 
 class AudioPlayerCubit extends Cubit<AudioPlayerState> {
   final AudioRepo _audioRepo;
+  final RecitersRepo _recitersRepo;
   final LoadSurahWithAudioUseCase _loadSurahWithAudioUseCase;
 
   AudioPlayerCubit({
     required AudioRepo audioRepo,
+    required RecitersRepo recitersRepo,
     required LoadSurahWithAudioUseCase loadSurahWithAudioUseCase,
   }) : _loadSurahWithAudioUseCase = loadSurahWithAudioUseCase,
        _audioRepo = audioRepo,
+       _recitersRepo = recitersRepo,
        super(const AudioPlayerState.initial()) {
     _listenToAudioEvents();
   }
@@ -31,14 +36,26 @@ class AudioPlayerCubit extends Cubit<AudioPlayerState> {
     final result = await _loadSurahWithAudioUseCase.call(surahId: surahId);
     result.fold(
       (failure) {
-        emit(AudioPlayerState.failed(failure: failure));
+        emit(AudioPlayerState.getSurahFailed(failure: failure));
       },
       (surah) async {
         _currentSurahNumber = surahId;
+        _getCurrentReciter();
         emit(AudioPlayerState.getSurahSuccess(surah: surah));
         await playOrPause();
       },
     );
+  }
+
+  ReciterModel? _currentReciter;
+  ReciterModel? get currentReciter => _currentReciter;
+
+  Future<void> _getCurrentReciter() async {
+    final result = await _recitersRepo.getCurrentReciter();
+    result.fold((_) {}, (reciter) {
+      _currentReciter = reciter;
+      emit(AudioPlayerState.reciterLoaded(reciter: reciter));
+    });
   }
 
   Future<void> getNextSurah() async {
@@ -72,11 +89,7 @@ class AudioPlayerCubit extends Cubit<AudioPlayerState> {
   }
 
   Future<void> seek({required Duration position}) async {
-    final result = await _audioRepo.seek(position: position);
-    result.fold(
-      (failure) => emit(AudioPlayerState.failed(failure: failure)),
-      (_) => {},
-    );
+    await _audioRepo.seek(position: position);
   }
 
   void _listenToAudioEvents() {
---
+++ lib/features/settings/presentation/view_models/settings_cubit/reciters_cubit.dart
@@ -0,0 +1,52 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:noor/core/data/services/shared_prefs_service.dart';
+import 'package:noor/core/error_handling/failure.dart';
+import 'package:noor/core/data/models/reciter_model/reciter_model.dart';
+import 'package:noor/features/settings/data/repos/reciters_repo.dart';
+
+part 'reciters_state.dart';
+part 'reciters_cubit.freezed.dart';
+
+class RecitersCubit extends Cubit<RecitersState> {
+  final SharedPrefsService _sharedPrefsService;
+  final RecitersRepo _recitersRepo;
+  RecitersCubit({
+    required SharedPrefsService sharedPrefsService,
+    required RecitersRepo recitersRepo,
+  }) : _sharedPrefsService = sharedPrefsService,
+       _recitersRepo = recitersRepo,
+       super(const RecitersState.initial()) {
+    getCurrentReciter();
+    getReciters();
+  }
+
+  ReciterModel? currentReciter;
+  List<ReciterModel>? reciters;
+
+  Future<void> getReciters() async {
+    final result = await _recitersRepo.getReciters();
+    result.fold((failure) => emit(RecitersState.getRecitersFailed(failure)), (
+      allReciters,
+    ) {
+      reciters = allReciters;
+      emit(const RecitersState.getRecitersSuccess());
+    });
+  }
+
+  Future<void> selectReciter({String? reciterIdentifier}) async {
+    if (reciterIdentifier == null) return;
+    await _sharedPrefsService.setReciterIdentifier(reciterIdentifier);
+    emit(const RecitersState.reciterSelected());
+  }
+
+  Future<void> getCurrentReciter() async {
+    final reciter = await _recitersRepo.getCurrentReciter();
+    reciter.fold((failure) => emit(RecitersState.reciterFailed(failure)), (
+      reciter,
+    ) {
+      currentReciter = reciter;
+      emit(const RecitersState.reciterLoaded());
+    });
+  }
+}
