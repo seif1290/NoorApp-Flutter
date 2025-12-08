c111bf2577881dc8a232555c66a689158d45d400
Seif Elnawawy <elnawawyseif@gmail.com>
2025-12-08 11:52:55 +0200

    added setReciterIdentifier and reciterIdentifier to shared preferences service

---
+++ lib/core/data/services/shared_prefs_service.dart
@@ -0,0 +1,26 @@
+import 'package:noor/core/utils/app_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefsService {
+  final SharedPreferences _prefs;
+  SharedPrefsService(this._prefs);
+
+  Future<bool> startOnBoarding() async =>
+      await _prefs.setBool(AppKeys.isOnboardingFinished, false);
+
+  Future<bool> finishOnBoarding() async =>
+      await _prefs.setBool(AppKeys.isOnboardingFinished, true);
+
+  Future<bool> setLanguageCode({required String languageCode}) async =>
+      await _prefs.setString(AppKeys.appLanguageCode, languageCode);
+
+  String? get appLanguageCode => _prefs.getString(AppKeys.appLanguageCode);
+
+  bool get isOnboardingFinished =>
+      _prefs.getBool(AppKeys.isOnboardingFinished) ?? false;
+
+  Future<bool> setReciterIdentifier(String identifier) async =>
+      await _prefs.setString(AppKeys.reciterIdentifier, identifier);
+
+  String? get reciterIdentifier => _prefs.getString(AppKeys.reciterIdentifier);
+}
