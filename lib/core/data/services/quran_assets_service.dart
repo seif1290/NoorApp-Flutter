db30d997feac0f87fed82a6dd11810a7439cc94d
Seif Elnawawy <elnawawyseif@gmail.com>
2025-12-08 11:54:36 +0200

    replace quran data source with quran assets service to share it in the app

--- lib/core/data/services/quran_assets_service.dart
+++ lib/core/data/services/quran_assets_service.dart
@@ -1,20 +1,16 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:noor/core/data/models/reciter_model/reciter_model.dart';
 import 'package:noor/core/error_handling/assets_failure.dart';
 import 'package:noor/features/home/data/models/surah_metadata_model/surah_metadata_model.dart';
 import 'package:noor/features/home/data/models/surah_model/surah_model.dart';
 
-abstract class QuranDataSource {
-  Future<List<SurahMetadataModel>> getQuranMetadata();
-  Future<SurahModel> getSurah({required int surahId});
-}
-
-class QuranDataSourceImpl implements QuranDataSource {
+class QuranAssetsService {
   final String _quranMetadataPath = 'assets/quran/metadata.json';
   final String _surahBase = 'assets/quran/surahs/';
+  final String _recitersPath = 'assets/quran/reciters.json';
 
-  @override
   Future<List<SurahMetadataModel>> getQuranMetadata() async {
     try {
       final String data = await rootBundle.loadString(_quranMetadataPath);
@@ -27,7 +23,6 @@ class QuranDataSourceImpl implements QuranDataSource {
     }
   }
 
-  @override
   Future<SurahModel> getSurah({required int surahId}) async {
     try {
       final String data = await rootBundle.loadString(
@@ -35,6 +30,28 @@ class QuranDataSourceImpl implements QuranDataSource {
       );
       final Map<String, dynamic> jsonMap = json.decode(data);
       return SurahModel.fromJson(jsonMap);
+    } catch (e) {
+      throw AssetFailure.fromException(e);
+    }
+  }
+
+  Future<List<ReciterModel>> getReciters() async {
+    try {
+      final String data = await rootBundle.loadString(_recitersPath);
+      final List<dynamic> jsonList = json.decode(data);
+      return jsonList.map((reciter) => ReciterModel.fromJson(reciter)).toList();
+    } catch (e) {
+      throw AssetFailure.fromException(e);
+    }
+  }
+
+  Future<ReciterModel> getReciter({required String identifier}) async {
+    try {
+      final List<ReciterModel> reciters = await getReciters();
+      return reciters.firstWhere(
+        (reciter) => reciter.identifier == identifier,
+        orElse: () => throw AssetFailure.unknown(),
+      );
     } catch (e) {
       throw AssetFailure.fromException(e);
     }
